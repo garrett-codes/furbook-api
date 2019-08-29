e8bb0539b217b438f7fe682199e10b6758948d6f
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-29 17:57:22 -0400

    modified

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+
+	def index
+		@photos = Photo.all
+		render :index
+	end
+
+	
+	private 
+
+	def photo_params
+    params.require(:photo).permit(:user_id, :picture)
+	end
+end
