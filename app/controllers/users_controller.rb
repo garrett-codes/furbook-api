ec191d8c08bd089c527aa909c5a6e42ea017724b
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-29 17:57:43 -0400

    modified

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+	def create
+		user = User.create(user_params)
+		# byebug
+		if user.valid?
+			render json: {token: encode_token(user_payload(user))}
+		else
+			render json: { errors: user.errors.full_messages }
+		end
+	end
+
+	# def show
+	# 	@user = User.find(params[:id])
+	# 	render :show
+	# end
+
+	def profile
+		# byebug
+		render json: current_user 
+	end
+
+	private 
+
+	def user_params
+		params.permit(:username, :password, :first_name, :last_name, :email)
+	end
+end
