d5a6c3b7a4ee1ad9f518146ebc178052d4947fc9
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-31 11:26:06 -0400

    pro pic controller modified

--- app/controllers/pro_pics_controller.rb
+++ app/controllers/pro_pics_controller.rb
@@ -1,5 +1,23 @@
 class ProPicsController < ApplicationController
-	def pro_pic_params
-    params.permit(:picture)
+	def index
+		@pro_pics = ProPic.all
+		render :index
+	end
+
+	def create
+		debugger
+		pro_pic = ProPic.new(photo_params)
+		if pro_pic.save
+			render json: {message: "You did it!"}
+		else
+			render json: pro_pic.errors.full_messages
+		end
+	end
+
+	
+	private 
+
+	def photo_params
+    params.require(:pro_pic).permit(:user_id, :picture)
 	end
 end
