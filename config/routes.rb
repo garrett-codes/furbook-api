ce5f25bc535ecb56db3a2903a620c30955276006
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-03 11:55:35 -0400

    routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :photos
   resources :pro_pics
+  resources :users
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/login", to: "auth#login"
 	post "/signup", to: "users#create"
-	get "/profile", to: "users#profile"
+	get "/profile", to: "users#profile" 
 end
