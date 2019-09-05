cd04b43fca41341eb1efb731da27f0b609a88fc8
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-04 20:19:48 -0400

    modififed comment serializer to include more info

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :post_id
+  attributes :id, :content, :user_id, :post_id, :created_at
   has_one :post
   has_one :user
 end
