49d6883ba275b64644d2e0baeb996fddab6188db
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-08 02:35:19 -0400

    updated

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,17 +1,18 @@
 class AuthController < ApplicationController
-	skip_before_action :authorized
+	skip_before_action :authorized, only: :login
 	
 	def login
 		user = User.find_by(username: params[:username])
 				# byebug
+
 		is_authenticated = user.authenticate(params[:password])
 
 		if is_authenticated
 			# payload = { user_id: user.id }
 
 			# token = JWT.encode payload, 'secret', 'HS256'
-
-			render json: {token: encode_token(user_payload(user))}
+			
+			render json: {token: encode_token(user)}
 		else
 			render json: {error: "Wrong username and/or password. Sorry!"}
 		end
