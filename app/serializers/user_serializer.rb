8b3850010f032db1409a69bc8e843ac6eed6ab0f
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-08 22:06:26 -0400

    serializer working

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :email, :first_name, :last_name
   has_many :posts
   has_many :comments, through: :posts
-  has_one :pro_pic
+  has_many :pro_pic
   has_many :photos
 
 end
