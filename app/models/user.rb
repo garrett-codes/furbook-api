53164fa5dc627e91517a7ff6385897da51569e79
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-09 10:08:20 -0400

    user freindship relationship[ defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
 	has_many :pro_pic, dependent: :destroy
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
+	has_and_belongs_to_many :friendships,
+		class_name: "User",
+		join_table: :freindships,
+		foreign_key: :user_id,
+		association_foreign_key: :friend_user_id
 end
