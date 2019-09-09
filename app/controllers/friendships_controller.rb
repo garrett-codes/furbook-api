894f4e7708f1f18d8d62f8764993fe6a5f6ded25
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-09 10:57:24 -0400

    up to date

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,33 @@
 class FriendshipsController < ApplicationController
+	def create
+		friendship = Friendship.create(friendship_params)
+		# byebug
+		if friendship.valid?
+			render json: {token: encode_token(friendship)}
+		else
+			render json: { errors: friendship.errors.full_messages }
+		end
+	end
+
+	def index
+		friendships = Friendship.all
+		render json: friendships
+	end
+
+	def show
+	  friendship_id = params[:id]
+	  friendship = Friendship.find(friendship_id)
+	  render json: friendship
+  end
+
+	# def show
+	# 	@user = User.find(params[:id])
+	# 	render :show
+	# end
+
+	private 
+
+	def friendship_params
+		params.permit(:user_id, :, :friend_user_id, :pending)
+	end
 end
