6dada36285fd6531bfcee0518f3502c22b49a5b3
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 10:33:37 -0400

    made migration

--- db/migrate/20190910142339_create_message_containers.rb
+++ db/migrate/20190910142339_create_message_containers.rb
@@ -1,7 +1,7 @@
 class CreateMessageContainers < ActiveRecord::Migration[5.2]
   def change
     create_table :message_containers do |t|
-
+    	t.references :friendship, foreign_key: true
       t.timestamps
     end
   end
