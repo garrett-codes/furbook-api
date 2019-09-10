7f5c625b07737d2b00bc2e518e4b3ff5aba8f3f9
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 17:11:04 -0400

    message controller working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,30 @@
 class MessagesController < ApplicationController
-	belongs_to :user
-	belongs_to :friendship
+	def create
+		message = Message.create(message_params)
+		# byebug
+		if message.valid?
+      # Return that post
+      render json: message
+    else
+      # Give the user error messages
+      render json: { errors: message.errors.full_messages }, status: :not_acceptable
+    end
+	end
+
+	def index
+		messages = Message.all
+		render json: comments
+	end
+
+	def show
+	  message_id = params[:id]
+	  message = Message.find(message_id)
+	  render json: message
+  end
+
+	private
+
+	def message_params
+		params.permit(:user_id, :friendship_id, :content)
+	end
 end
