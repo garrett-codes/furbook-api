7cb334395dea15fb6372f773bb253af817a53249
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-10 19:14:17 -0400

    everything is working well

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,9 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :message_user
   has_one :user
   has_one :friendship
+
+  def message_user
+  	object.user
+  end
 end
